d7387495ce86ab1ec87d66033d0c347a4daee71d
Shaun Rodgers <52825780+sciados@users.noreply.github.com>
2025-06-11 11:56:12 +0100

    Update files

---
+++ src/shared/context/SupabaseContext.jsx
@@ -0,0 +1,9 @@
+// src/context/SupabaseContext.js
+import { createContext } from 'react';
+
+/**
+ * Context for Supabase authentication and data access
+ */
+const SupabaseContext = createContext(null);
+
+export default SupabaseContext;
